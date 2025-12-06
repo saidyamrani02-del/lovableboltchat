6244052887412159b27c6c06e4a85c782765c5c3
saidyamrani02-del <saidyamrani02@gmail.com>
2025-12-06 09:26:41 +0300

    Delete supabase directory

--- supabase/migrations/20251108063956_a0c12382-d6dc-4f7d-9bec-555fdebaca7a.sql
+++
@@ -1,8 +0,0 @@
--- Allow admins to update any wallet
-CREATE POLICY "Admins can update any wallet"
-ON public.wallets
-FOR UPDATE
-USING (has_role(auth.uid(), 'admin'::app_role));
-
--- Enable realtime for video_calls table so recipients get notified
-ALTER PUBLICATION supabase_realtime ADD TABLE public.video_calls;
--- supabase/migrations/20251205064918_5fd2d63f-6cf4-44f3-b667-316cb41bc3c6.sql
+++
@@ -1,4 +0,0 @@
--- Add Metered API settings columns to api_settings table
-ALTER TABLE public.api_settings 
-ADD COLUMN IF NOT EXISTS metered_app_name TEXT,
-ADD COLUMN IF NOT EXISTS metered_secret_key TEXT;
